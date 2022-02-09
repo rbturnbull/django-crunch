1e93ddc08467455f3f99c0ff72ab82755f2021ef
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2022-02-09 16:30:40 +1100

    auto importing admin

--- crunch/django/app/apps.py
+++ crunch/django/app/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
-
+from importlib import import_module
 
 class CrunchConfig(AppConfig):
     name = "crunch"
+
+    def ready(self):
+        super().ready()
+        import_module('crunch.django.app.admin') # needed because the autodiscover doesn't work unless admin.py is in top directory of app
