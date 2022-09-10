17ebb196a696d14dc615d280cfc6c8a90f847e95
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2022-09-10 12:14:10 +1000

    :cop: adding test for diagnostics

--- tests/test_client_main.py
+++ tests/test_client_main.py
@@ -10,3 +10,9 @@ class ClientMainTestCase(TestCase):
         result = runner.invoke(app, ["--help"])
         assert result.exit_code == 0
         assert "Usage: " in result.stdout
+
+    def test_diagnostics(self):
+        result = runner.invoke(app, ["diagnostics"])
+        assert result.exit_code == 0
+        assert "agent_user" in result.stdout
+        assert "version" in result.stdout
