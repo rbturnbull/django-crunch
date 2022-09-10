90a6391687196eb446750742f7408d32683323fa
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2022-09-10 12:15:06 +1000

    :zap: splitting datasets on project detail page to different categories

--- crunch/templatetags/crunch.py
+++ crunch/templatetags/crunch.py
@@ -5,13 +5,30 @@ from django import template
 
 register = template.Library()
 
+
 class ItemMapTag(InclusionTag):
     name = "item_map"
-    template = 'crunch/item_map.html'
+    template = "crunch/item_map.html"
     options = Options(
-        Argument('item'),
+        Argument("item"),
     )
+
     def get_context(self, context, item):
-        return {'item': item}
+        return {"item": item}
+
 
 register.tag(ItemMapTag)
+
+
+class DatasetListTag(InclusionTag):
+    name = "dataset_list"
+    template = "crunch/dataset_list.html"
+    options = Options(
+        Argument("datasets"),
+    )
+
+    def get_context(self, context, datasets):
+        return {"datasets": datasets}
+
+
+register.tag(DatasetListTag)
